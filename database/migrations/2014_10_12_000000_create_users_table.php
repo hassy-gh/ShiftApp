4b21ca2904a71503736820cc5879095cfcda9a9a
hassy-gh <shufc0107@gmail.com>
2022-06-28 16:16:56 +0900

    SA-11 【従業員】新規登録、認証機能の実装 サンプルデータの作成

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('user_name')->unique()->comment('アカウントID');
+            $table->string('last_name')->comment('氏名（姓）');
+            $table->string('first_name')->comment('氏名（名）');
+            $table->string('email')->unique()->comment('メールアドレス');
+            $table->timestamp('email_verified_at')->nullable()->comment('認証日時');
+            $table->string('password')->comment('パスワード');
+            $table->rememberToken()->comment('情報保持トークン');
             $table->timestamps();
+            $table->softDeletes()->comment('削除日時');
         });
     }
 
