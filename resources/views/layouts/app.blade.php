6e26411287c66581c1c0430d154e48f40cd0b106
hassy-gh <shufc0107@gmail.com>
2022-07-08 14:35:26 +0900

    SA-15 TOPページの編集

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,62 +23,48 @@
 
 <body>
   <div id="app">
-    @auth
-    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-      <div class="container">
-        <a class="navbar-brand text-primary" href="{{ url('/') }}">
-          {{ config('app.name', 'Laravel') }}
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
+      <div class="container-fluid">
+        <h1 class="text-primary">{{ config('app.name') }}</h1>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll"
+          aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <!-- Left Side Of Navbar -->
-          <ul class="navbar-nav me-auto">
-
-          </ul>
-
-          <!-- Right Side Of Navbar -->
-          <ul class="navbar-nav ms-auto">
-            <!-- Authentication Links -->
+        <div class="collapse navbar-collapse" id="navbarScroll">
+          <ul class="navbar-nav ms-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
             @guest
-            @if (Route::has('login'))
             <li class="nav-item">
-              <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+              <a class="nav-link" aria-current="page" href="#">機能</a>
             </li>
-            @endif
-
-            @if (Route::has('register'))
             <li class="nav-item">
-              <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+              <a class="nav-link" href="#">使い方</a>
             </li>
-            @endif
+            <li class="nav-item">
+              <a class="nav-link" href="#">Q&A</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" href="{{ route('admin.login') }}">管理者の方</a>
+            </li>
             @else
-            <li class="nav-item dropdown">
-              <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                aria-haspopup="true" aria-expanded="false" v-pre>
-                {{ Auth::user()->name }}
-              </a>
 
-              <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                  {{ __('Logout') }}
-                </a>
-
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                  @csrf
-                </form>
-              </div>
-            </li>
             @endguest
           </ul>
+          @guest
+          <a href="{{ route('login') }}" class="btn btn-light mx-3">ログイン</a>
+          <a href="{{ route('register') }}" class="btn btn-primary">会員登録</a>
+          @else
+          <a href="{{ route('logout') }}" class="btn btn-primary" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+            {{ __('Logout') }}
+          </a>
+
+          <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+            @csrf
+          </form>
+          @endguest
         </div>
       </div>
     </nav>
-    @endauth
 
     <main class="py-4">
       @yield('content')
